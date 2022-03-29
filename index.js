0db36f694a6ab4435bb43df29a3a29fab7ccc612
rohankewal <rkewalramani4@gmail.com>
2022-03-29 18:19:12 -0400

    imported header component

---
+++ index.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+import Header from "./components/header";
+
+export default function Index() {
+    return (
+        <div className="container">
+            <Header />
+        </div>
+    )
+}
