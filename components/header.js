3f3f17b524fca89f2d930bf3aa964d97331c7e7b
rohankewal <rkewalramani4@gmail.com>
2022-03-29 18:18:46 -0400

    created a header component

---
+++ components/header.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className="text">Social Scrape</h1>
+        </div>
+    )
+}
